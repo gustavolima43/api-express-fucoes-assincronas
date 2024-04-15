1a6fbb114c2178aed6cfb818885f1db5979c1698
Gustavo Lima <gustavolimamendes2@gmail.com>
2024-04-15 18:00:58 -0300

    Promise all

--- src/index.js
+++ src/index.js
@@ -4,9 +4,13 @@ const { getCityFromZipcode } = require('utils-playground');
 const app = express();
 
 app.get('/', async (req, res) => {
+    const cidade = await getCityFromZipcode('40231015');
+    const cidade2 = await getCityFromZipcode('44700000');
     
-    const cidade = await getCityFromZipcode('44700000');
-    res.send(`A cidade encontrada foi: ${cidade}`);
+    const promise = await Promise.all([cidade, cidade2]);
+
+    const [ resposta1, resposta2 ] = promise;
+    res.send(`A cidade encontrada foi: ${resposta2} e ${resposta1}`);
 });
 
 app.listen(3000);
